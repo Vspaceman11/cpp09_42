3b24dc8a926ac8b547bcad30c17fa8aa81df30a6
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2026-06-08 15:08:31 +0200

    feat: implement calculate method

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -27,27 +27,66 @@ bool RPN::performOperation(char op)
 	if (_stack.size() < 2)
 		return false;
 
-		int second = _stack.back();
-		_stack.pop_back();
+	int second = _stack.back();
+	_stack.pop_back();
 
-		int first = _stack.back();
-		_stack.pop_back();
+	int first = _stack.back();
+	_stack.pop_back();
 
-		int result = 0;
-		if (op == '+')
-			result = first + second;
-		else if (op == '-')
-			result = first - second;
-		else if (op == '*')
-			result = first * second;
-		else if (op == '/')
-		{
-			if (second == 0)
-				return false;
-			result = first / second;
-		}
+	int result = 0;
+	if (op == '+')
+		result = first + second;
+	else if (op == '-')
+		result = first - second;
+	else if (op == '*')
+		result = first * second;
+	else if (op == '/')
+	{
+		if (second == 0)
+			return false;
+		result = first / second;
+	}
 
-		_stack.push_back(result);
-		return true;
+	_stack.push_back(result);
+	return true;
 }
 
+void RPN::calculate(const std::string& expression)
+{
+	for (size_t i = 0; i < expression.length(); ++i)
+	{
+		char c = expression[i];
+
+		if (std::isspace(c))
+			continue;
+
+		if (std::isdigit(c))
+		{
+			if (i + 1 < expression.length() && std::isdigit(expression[i + 1]))
+			{
+				std::cerr << "Error" << std::endl;
+				return;
+			}
+			_stack.push_back(c - '0');
+		}
+		else if (isOperator(c))
+		{
+			if (!performOperation(c))
+			{
+				std::cerr << "Error" << std::endl;
+				return;
+			}
+		}
+		else
+		{
+			std::cerr << "Error" << std::endl;
+			return;
+		}
+	}
+	if (_stack.size() != 1)
+	{
+		std::cerr << "Error" << std::endl;
+		return;
+	}
+	std::cout << _stack.back() << std::endl;
+}
