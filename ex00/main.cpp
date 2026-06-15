5231a4322245335832f5ae3dc4d75e756ab043eb
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2026-06-15 15:08:42 +0200

    fix: shift brackets

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,19 +1,23 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-	if (argc != 2) {
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
 		std::cerr << "Error: could not open file.\n";
 		return 1;
 	}
 
-	try {
+	try
+	{
 		BitcoinExchange exchange;
 		exchange.processInputFile(argv[1]);
-	} catch (const std::exception& e) {
+	}
+	catch (const std::exception& e)
+	{
 		std::cerr << e.what() << "\n";
 		return 1;
 	}
-
 	return 0;
 }
