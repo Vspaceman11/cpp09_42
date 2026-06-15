ea4d2099a84c06940379d08cee3991f013157b7e
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2026-06-15 17:18:55 +0200

    fix: shift brackets

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,8 +1,10 @@
 #include "RPN.hpp"
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-	if (argc != 2) {
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
 		std::cerr << "Error" << std::endl;
 		return 1;
 	}
